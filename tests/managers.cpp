395c295c57ae6eadd82da43d74507eabef8c84de
Erik Parreira <etinlb@gmail.com>
2019-03-30 13:28:33 -0700

    Helper functions make writing tests a bit easier

--- tests/managers.cpp
+++ tests/managers.cpp
@@ -12,12 +12,60 @@
 #include <vector>
 using namespace Catch::literals;
 
-class TestPacket : Packet
+// Round about way to see if the packet written contains all the expected
+// messages
+void checkMessageId(std::shared_ptr<Packet> packet, PacketSerializer packetSerializer,
+                    int messageIdStart, int numMessages)
 {
-  public:
-    TestPacket();
-    ~TestPacket();
-};
+    auto out = OutputMemoryBitStream();
+    packetSerializer.WritePacket(packet, out);
+    int messageId = messageIdStart;
+
+    auto rawChar = out.GetBufferPtr();
+    auto in = InputMemoryBitStream(rawChar, out.GetBitLength());
+    auto packets = packetSerializer.ReadPackets(in);
+
+    for (int i = 0; i < packets.size(); ++i)
+    {
+        auto castRelPacket = static_cast<ReliableOrderedPacket*>(packets[i].get());
+        REQUIRE(castRelPacket->messages->size() == numMessages);
+
+        for (int i = 0; i < numMessages; ++i)
+        {
+            auto message = castRelPacket->messages->at(i);
+            REQUIRE(message->GetId() == messageId);
+            messageId++;
+        }
+    }
+}
+
+// helper that has the manager read a packet that contains only ack data
+void ackPacket(PacketManager& manager, PacketSerializer packetSerializer, int ackNum, int ackBits,
+               int sequenceNumber)
+{
+    auto ackedPacket = packetSerializer.CreatePacket(ReliableOrderedPacket::ID);
+    auto castPacket = std::static_pointer_cast<ReliableOrderedPacket>(ackedPacket);
+    // say we have seen packet 0 by setting the ack and bit field appropriately
+    castPacket->ack = ackNum;
+    castPacket->ackBits = ackBits;
+
+    // give it our own starting sequence number of 0
+    castPacket->sequenceNumber = sequenceNumber;
+    // no messages in this packet
+    castPacket->numMessages = 0;
+    manager.ReadPacket(castPacket);
+}
+
+void sendMessages(PacketManager& manager, MessageSerializer messageSerializer, uint8_t numMessages)
+{
+    // write out a packet with 5 messages
+    for (int i = 0; i < numMessages; ++i)
+    {
+        auto msg = std::static_pointer_cast<PlayerMessage>(
+            std::move(messageSerializer.CreateMessage(PlayerMessage::ID)));
+        manager.SendMessage(msg);
+    }
+}
 
 TEST_CASE("Packet Manager", "[manger]")
 {
@@ -31,121 +79,38 @@ TEST_CASE("Packet Manager", "[manger]")
     auto clientManager = PacketManager(packetSerializer);
     auto serverManager = PacketManager(packetSerializer);
 
-    // Stream data stuffs
-    // auto packetPtr = std::make_shared<std::vector<uint8_t>>(fullPackets);
+    sendMessages(clientManager, *messageSerializer, 5);
+    auto packet = clientManager.WritePacket();
+    checkMessageId(packet, packetSerializer, 0, 5);
 
-    // write out a packet with 5 messages
-    for (int i = 0; i < 5; ++i)
-    {
-        auto msg = std::static_pointer_cast<PlayerMessage>(
-            std::move(messageSerializer->CreateMessage(PlayerMessage::ID)));
-        clientManager.SendMessage(msg);
-    }
-    auto firstPacket = clientManager.WritePacket();
+    sendMessages(clientManager, *messageSerializer, 3);
+    packet = clientManager.WritePacket();
+    checkMessageId(packet, packetSerializer, 5, 3);
 
-    // write second packet with 3 messages
-    for (int i = 0; i < 3; ++i)
-    {
-        auto msg = std::static_pointer_cast<PlayerMessage>(
-            std::move(messageSerializer->CreateMessage(PlayerMessage::ID)));
-        clientManager.SendMessage(msg);
-        /* code */
-    }
-
-    auto secondPacket = clientManager.WritePacket();
-
-    auto out = OutputMemoryBitStream();
-    // check the first packet is good
-    packetSerializer.WritePacket(firstPacket, out);
-
-    auto rawChar = out.GetBufferPtr();
-    auto in = InputMemoryBitStream(rawChar, out.GetBitLength());
-    auto packets = packetSerializer.ReadPackets(in);
-
-    int messageId = 0;
-
-    // basic did it write out the 5 messages
-    for (int i = 0; i < packets.size(); ++i)
-    {
-        auto castRelPacket = static_cast<ReliableOrderedPacket*>(packets[i].get());
-        REQUIRE(castRelPacket->messages->size() == 5);
-        for (auto msg : *castRelPacket->messages)
-        {
-            REQUIRE(msg->GetId() == messageId);
-            messageId++;
-        }
-    }
-
-    REQUIRE(messageId == 5);
-
-    // check the second packet
-    out = OutputMemoryBitStream();
-    packetSerializer.WritePacket(secondPacket, out);
-
-    rawChar = out.GetBufferPtr();
-    in = InputMemoryBitStream(rawChar, out.GetBitLength());
-    packets = packetSerializer.ReadPackets(in);
-
-    // writes out the next 3 messages
-    for (int i = 0; i < packets.size(); ++i)
-    {
-        auto castRelPacket = static_cast<ReliableOrderedPacket*>(packets[i].get());
-        REQUIRE(castRelPacket->messages->size() == 3);
-        for (auto msg : *castRelPacket->messages)
-        {
-            REQUIRE(msg->GetId() == messageId);
-            messageId++;
-        }
-    }
-
-    messageId = 0;
     // advance just a little past our threshold of .1
-    clientManager.AdvanceTime(0.11);
+    clientManager.SetTime(0.11);
 
-    auto thirdPacket = clientManager.WritePacket();
+    packet = clientManager.WritePacket();
+    // Since we advanced time, the third packet will have all 8 messages since they
+    // are unacked
+    checkMessageId(packet, packetSerializer, 0, 8);
 
-    out = OutputMemoryBitStream();
-    // check the first packet is good
-    packetSerializer.WritePacket(thirdPacket, out);
+    // Start acking some packets
+    // pretend to receive a packet from something that says we have received
+    // the 0th packet
+    ackPacket(clientManager, packetSerializer, 0, 1, 0);
 
-    rawChar = out.GetBufferPtr();
-    in = InputMemoryBitStream(rawChar, out.GetBitLength());
-    packets = packetSerializer.ReadPackets(in);
+    // move time ahead
+    clientManager.SetTime(0.22);
+    packet = clientManager.WritePacket();
+    // fourth packet contains messages not included in the 0th packet
+    checkMessageId(packet, packetSerializer, 5, 3);
 
-    // should write all un ack'd messages
-    for (int i = 0; i < packets.size(); ++i)
-    {
-        auto castRelPacket = static_cast<ReliableOrderedPacket*>(packets[i].get());
-        REQUIRE(castRelPacket->messages->size() == 8);
-        for (auto msg : *castRelPacket->messages)
-        {
-            REQUIRE(msg->GetId() == messageId);
-            messageId++;
-        }
-    }
+    // ack second packet
+    ackPacket(clientManager, packetSerializer, 1, 2, 1);
 
-    // Start acking some messages
-
-    // if we advance time, the next packet will include all the packets
-    // that aren't ack'd, which should be all of them at this point
-
-    // // next packe twill write the
-    // packet = clientManager.WritePacket();
-
-    // packetSerializer.WritePacket(packet, out);
-
-    // rawChar = out.GetBufferPtr();
-    // in = InputMemoryBitStream(rawChar, out.GetBitLength());
-    // packets = packetSerializer.ReadPackets(in);
-
-    // // basic did it put the messages into the thing
-    // for (int i = 0; i < packets.size(); ++i)
-    // {
-    //     auto castRelPacket = static_cast<ReliableOrderedPacket*>(packets[i].get());
-    //     for (auto msg : *castRelPacket->messages)
-    //     {
-    //         REQUIRE(msg->GetId() == messageId);
-    //         messageId++;
-    //     }
-    // }
+    // There shouldn't be any more messages to send
+    packet = clientManager.WritePacket();
+    // ack'd all packets, should have written no messages
+    checkMessageId(packet, packetSerializer, 0, 0);
 }
