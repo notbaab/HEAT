910f9986c59a410cd9ee5bf2b5cfd16f2d4feae6
Erik Parreira <etinlb@gmail.com>
2019-03-31 21:25:01 -0700

    Testing back and forth from client server

--- tests/managers.cpp
+++ tests/managers.cpp
@@ -71,48 +71,76 @@ void sendMessages(PacketManager& manager, std::shared_ptr<MessageSerializer> mes
 
 TEST_CASE("Packet Manager", "[manger]")
 {
-    // setup our serializers
+
     auto messageSerializer = std::make_shared<MessageSerializer>();
     AddMessageCtor(messageSerializer, PlayerMessage);
     auto packetSerializer = std::make_shared<PacketSerializer>(messageSerializer);
     AddPacketCtor(packetSerializer, ReliableOrderedPacket);
 
-    // Managers
-    auto clientManager = PacketManager(packetSerializer);
-    auto serverManager = PacketManager(packetSerializer);
+    SECTION("Basic packet and message ack")
+    {
+        // Managers
+        auto manager = PacketManager(packetSerializer);
 
-    sendMessages(clientManager, *messageSerializer, 5);
-    auto packet = clientManager.WritePacket();
-    checkMessageId(packet, packetSerializer, 0, 5);
+        sendMessages(manager, messageSerializer, 5);
+        auto packet = manager.WritePacket();
+        checkMessageId(packet, packetSerializer, 0, 5);
 
-    sendMessages(clientManager, *messageSerializer, 3);
-    packet = clientManager.WritePacket();
-    checkMessageId(packet, packetSerializer, 5, 3);
+        sendMessages(manager, messageSerializer, 3);
+        packet = manager.WritePacket();
+        checkMessageId(packet, packetSerializer, 5, 3);
 
-    // advance just a little past our threshold of .1
-    clientManager.SetTime(0.11);
+        // advance just a little past our threshold of .1
+        manager.SetTime(0.11);
 
-    packet = clientManager.WritePacket();
-    // Since we advanced time, the third packet will have all 8 messages since they
-    // are unacked
-    checkMessageId(packet, packetSerializer, 0, 8);
+        packet = manager.WritePacket();
+        // Since we advanced time, the third packet will have all 8 messages since they
+        // are unacked
+        checkMessageId(packet, packetSerializer, 0, 8);
 
-    // Start acking some packets
-    // pretend to receive a packet from something that says we have received
-    // the 0th packet
-    ackPacket(clientManager, packetSerializer, 0, 1, 0);
+        // Start acking some packets
+        // pretend to receive a packet from something that says we have received
+        // the 0th packet
+        ackPacket(manager, packetSerializer, 0, 1, 0);
 
-    // move time ahead
-    clientManager.SetTime(0.22);
-    packet = clientManager.WritePacket();
-    // fourth packet contains messages not included in the 0th packet
-    checkMessageId(packet, packetSerializer, 5, 3);
+        // move time ahead
+        manager.SetTime(0.22);
+        packet = manager.WritePacket();
+        // fourth packet contains messages not included in the 0th packet
+        checkMessageId(packet, packetSerializer, 5, 3);
 
-    // ack second packet
-    ackPacket(clientManager, packetSerializer, 1, 2, 1);
+        // ack second packet
+        ackPacket(manager, packetSerializer, 1, 2, 1);
 
-    // There shouldn't be any more messages to send
-    packet = clientManager.WritePacket();
-    // ack'd all packets, should have written no messages
-    checkMessageId(packet, packetSerializer, 0, 0);
+        // There shouldn't be any more messages to send
+        packet = manager.WritePacket();
+        // ack'd all packets, should have written no messages
+        checkMessageId(packet, packetSerializer, 0, 0);
+    }
+
+    SECTION("Client Server Test")
+    {
+        auto client = PacketManager(packetSerializer);
+        auto server = PacketManager(packetSerializer);
+
+        sendMessages(client, messageSerializer, 5);
+
+        auto packet = client.WritePacket();
+        checkMessageId(packet, packetSerializer, 0, 5);
+
+        // read packet
+        bool success = server.ReadPacket(packet);
+        REQUIRE(success);
+
+        // advance so it so server acks packet
+        server.SetTime(0.11);
+        packet = server.WritePacket();
+        success = client.ReadPacket(packet);
+
+        packet = client.WritePacket();
+
+        // next client packet won't send the olde messages
+        checkMessageId(packet, packetSerializer, 5, 0);
+        REQUIRE(success);
+    }
 }
