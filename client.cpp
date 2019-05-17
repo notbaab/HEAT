de7eba14295e47a072c05ae1a52f380653923c7e
Erik Parreira <etinlb@gmail.com>
2019-05-17 08:59:05 -0700

    Using the controls library in client now

--- client.cpp
+++ client.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <thread>
 
+#include "controls/InputManager.h"
 #include "graphics/GraphicsDriver.h"
 #include "graphics/RenderManager.h"
 #include "graphics/SpriteComponent.h"
@@ -136,80 +137,19 @@ bool startSDL()
     return true;
 }
 
-void HandleEvent(SDL_Event* event)
-{
-    //     std::cout << "In event " << event->type << std::endl;
-    //     if (event->type == SDL_WINDOWEVENT)
-    //     {
-    //         switch (event->window.event)
-    //         {
-    //         case SDL_WINDOWEVENT_SHOWN:
-    //             SDL_Log("Window %d shown", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_HIDDEN:
-    //             SDL_Log("Window %d hidden", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_EXPOSED:
-    //             SDL_Log("Window %d exposed", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_MOVED:
-    //             SDL_Log("Window %d moved to %d,%d", event->window.windowID, event->window.data1,
-    //                     event->window.data2);
-    //             break;
-    //         case SDL_WINDOWEVENT_RESIZED:
-    //             SDL_Log("Window %d resized to %dx%d", event->window.windowID,
-    //             event->window.data1,
-    //                     event->window.data2);
-    //             break;
-    //         case SDL_WINDOWEVENT_SIZE_CHANGED:
-    //             SDL_Log("Window %d size changed to %dx%d", event->window.windowID,
-    //             event->window.data1,
-    //                     event->window.data2);
-    //             break;
-    //         case SDL_WINDOWEVENT_MINIMIZED:
-    //             SDL_Log("Window %d minimized", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_MAXIMIZED:
-    //             SDL_Log("Window %d maximized", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_RESTORED:
-    //             SDL_Log("Window %d restored", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_ENTER:
-    //             SDL_Log("Mouse entered window %d", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_LEAVE:
-    //             SDL_Log("Mouse left window %d", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_FOCUS_GAINED:
-    //             SDL_Log("Window %d gained keyboard focus", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_FOCUS_LOST:
-    //             SDL_Log("Window %d lost keyboard focus", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_CLOSE:
-    //             SDL_Log("Window %d closed", event->window.windowID);
-    //             break;
-    // #if SDL_VERSION_ATLEAST(2, 0, 5)
-    //         case SDL_WINDOWEVENT_TAKE_FOCUS:
-    //             SDL_Log("Window %d is offered a focus", event->window.windowID);
-    //             break;
-    //         case SDL_WINDOWEVENT_HIT_TEST:
-    //             SDL_Log("Window %d has a special hit test", event->window.windowID);
-    //             break;
-    // #endif
-    //         default:
-    //             SDL_Log("Window %d got unknown event %d", event->window.windowID,
-    //             event->window.event); break;
-    //         }
-    //     }
-}
+void KeyPressed(int keyCode) { std::cout << "Key pressed " << keyCode << std::endl; }
+void KeyReleased(int keyCode) { std::cout << "Key pressed " << keyCode << std::endl; }
+
 bool Loop()
 {
     auto spriteData = SpriteSheetData(TESTSHEET, TESTDATA);
     auto simpleGameObject = SimpleGameObject(spriteData);
     auto spriteComponent = SpriteComponent(&simpleGameObject);
     RenderManager<SimpleGameObject>::sInstance->AddComponent(&spriteComponent);
+
+    InputManager::StaticInit();
+    InputManager::sInstance->RegisterKeyDownListner(KeyPressed);
+    InputManager::sInstance->RegisterKeyUpListner(KeyReleased);
 
     while (true)
     {
@@ -227,7 +167,7 @@ bool Loop()
             else
             {
                 simpleGameObject.changeAnimation(rand() % 3);
-                HandleEvent(&event);
+                InputManager::sInstance->HandleSDLEvent(event);
             }
         }
 
