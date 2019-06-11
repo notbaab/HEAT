8ed036e8fe4edebb7a2959d08c24a2364fdde3a3
ET <etinlb@gmail.com>
2019-06-10 20:47:17 -0700

    Convenient callback for printing network data

--- networking/SocketManager.h
+++ networking/SocketManager.h
@@ -7,10 +7,24 @@
 
 using RecieveCallback = std::function<void(std::shared_ptr<std::vector<uint8_t>>)>;
 
+namespace networking
+{
+void printCallback(std::shared_ptr<std::vector<uint8_t>> data)
+{
+    for (auto i = (*data).begin(); i != (*data).end(); ++i)
+    {
+        std::cout << std::hex << unsigned(*i) << '|';
+    }
+
+    std::cout << std::endl;
+}
+} // namespace networking
+
 class SocketManager
 {
   public:
     SocketManager(uint16_t port, RecieveCallback receiveCallback);
+    int SendTo(const void* data, int length, const SocketAddress& toAddress);
     void Stop();
 
   private:
