655ee193f3a0e15f67aaa84b4de9fd43c48c9622
ET <etinlb@gmail.com>
2020-03-22 17:34:20 -0700

    Adding get all commands to to the debug tools
    The idea is anyone can request what commands are available from the
    socket and feed that back to a user

--- debugging_tools/debug_commands.cpp
+++ debugging_tools/debug_commands.cpp
@@ -9,10 +9,15 @@ static std::unordered_map<std::string, DebugCommand> commands;
 void add_command(std::string command, DebugCommand callback)
 {
     commands.emplace(command, callback);
+    INFO("Adding {}|", command);
 }
 
 bool tryExecuteCommand(std::string command, std::vector<std::string> args, std::string* outInfo)
 {
+    if (args.size() != 0)
+    {
+        INFO("Executing {} with param {}", command, args[0]);
+    }
     if (commands.find(command) != commands.end())
     {
         try
@@ -20,23 +25,44 @@ bool tryExecuteCommand(std::string command, std::vector<std::string> args, std::
             *outInfo = commands[command](args);
             return true;
         }
-        catch (const std::exception& e)
+        // catch (const std::exception& e)
+        catch (const std::runtime_error& e)
         {
             ERROR("Exception thrown {}", e.what());
+            *outInfo = e.what();
             return false;
         }
     }
+    WARN("Command {} not found", command);
+
+    for (const auto& kv : commands)
+    {
+        WARN("{}", kv.first);
+    }
+
     return false;
 }
 
-std::vector<std::string> GetAllCommands()
+std::string GetAllCommands(std::vector<std::string>)
 {
-    std::vector<std::string> keys;
-    keys.reserve(commands.size());
+    INFO("Adding commands");
+    std::vector<std::string> commandVec;
+    commandVec.reserve(commands.size());
 
-    for (auto kv : commands)
+    for (const auto& kv : commands)
     {
-        keys.push_back(kv.first);
+        commandVec.push_back(kv.first);
     }
-    return keys;
+
+    std::string commandStr;
+    for (const auto& cmd : commandVec)
+    {
+        commandStr += cmd;
+        commandStr += "\n";
+    }
+    // commandStr.pop_back();
+
+    return commandStr;
 }
+
+void addGetAllCommands() { add_command("get-commands", GetAllCommands); }
--- debugging_tools/debug_commands.h
+++ debugging_tools/debug_commands.h
@@ -6,3 +6,4 @@
 using DebugCommand = std::function<std::string(std::vector<std::string>)>;
 void add_command(std::string command, DebugCommand function);
 bool tryExecuteCommand(std::string command, std::vector<std::string> args, std::string* outInfo);
+void addGetAllCommands();
